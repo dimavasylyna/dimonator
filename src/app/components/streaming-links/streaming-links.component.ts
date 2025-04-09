ae77a9925c6ec8de71194ac5e346ef864ddf64cd
d.vasylyna <dmytro.vasylyna@mgid.com>
2025-04-09 19:24:46 +0300

    update links

--- src/app/components/streaming-links/streaming-links.component.ts
+++ src/app/components/streaming-links/streaming-links.component.ts
@@ -11,14 +11,9 @@ import {IconComponent} from '../icon/icon.component';
 })
 export class StreamingLinksComponent {
   readonly links = [
-    {title: 'Spotify', url: 'https://...', name: 'spotify'},
-    {title: 'Apple Music', url: 'https://...', name: 'apple'},
-    {title: 'YouTube', url: 'https://...', name: 'youtube'},
-    {title: 'iTunes', url: 'https://...', name: 'itunes'},
-    {title: 'Amazon', url: 'https://...', name: 'amazon'},
+    {title: 'Spotify', url: 'https://open.spotify.com/album/4WadjJGHEpRUqDtNRY0WE3?si=SW68llGoTLiRmtTHrS7jDQ', name: 'spotify'},
+    {title: 'Apple Music', url: 'https://music.apple.com/ua/album/%D1%81%D1%82%D0%B0%D1%80%D0%B8%D0%B9-%D0%B4%D1%80%D1%83%D0%B3/1807182616?i=1807182617&l=uk', name: 'apple'},
+    {title: 'YouTube', url: 'https://music.youtube.com/watch?v=cCMK_zvHctY&si=PQI-jZ3Nf3r1LBOe', name: 'youtube'},
+    {title: 'Amazon', url: 'https://music.amazon.com/tracks/B0F3ZW64LW?marketplaceId=ATVPDKIKX0DER&musicTerritory=US&ref=dm_sh_RmC2BZDU7IuWHhqbCPe4KCgKQ', name: 'amazon'},
   ];
-
-  private getIconLink(icon: string) {
-    return `assets/icons/${icon}.svg`;
-  };
 }
